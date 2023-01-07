9f84d17b99e64951ff91e49456284ecd46db108b
jfrsbg <comerciall.jeferson@gmail.com>
2023-01-07 13:48:35 -0300

    [skip ci] new comment

--- events-deep-dive/src/components/HelpArea.jsx
+++ events-deep-dive/src/components/HelpArea.jsx
@@ -9,8 +9,8 @@ const HELP_ITEMS = [
   },
   {
     id: 'h2',
-    title: 'What is GitHub?',
-    text: 'GitHub is a company and online offering, providing you with tons of Git-related services (e.g., cloud repositories).',
+    title: 'Title',
+    text: 'text to describe',
   },
   {
     id: 'h3',
